e825d42ed53ae4268e8b5796056908b8df635377
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-05-21 17:20:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,7 +25,7 @@ ls upload
 n=0
 for i in "${array[@]}"
 do
-  echo ${array[@]}
+  echo ${array[n]}
   sed -i "s+</ul></li></ul></div></div>+<li class=\"toctree-l2\"><a class=\"reference internal\" href=\"${array[n]}/index.html\">${array[n]}</a></li></ul></li></ul></div></div>+" upload/index.html
   n=$n+1
 done
