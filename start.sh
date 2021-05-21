72bb6d85820a7ebe774d89c5cee0933dcd092807
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-05-21 16:54:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,6 +19,8 @@ git checkout origin/docs
 ls
 
 array=($(ls -d */))
+
+git checkout origin/main
 echo ${array[1]} 
 sed -i "s+</ul></li></ul></div></div>+<li class=\"toctree-l2\"><a class=\"reference internal\" href=\"$INPUT_BRANCHNAME/index.html\">$INPUT_BRANCHNAME</a></li></ul></li></ul></div></div>+" index.html
 
