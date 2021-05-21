d2a2539b8ea7b523cd7ceebccb6708897c7ffb56
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-05-21 16:08:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,3 +19,4 @@ git checkout origin/docs
 ls
 
 sed -i "s+<li class=\"toctree-l2\"><a class=\"reference internal\" href=\"{{BRANCHNAME}}/index.html\">{{BRANCHNAME}}</a></li></ul></li></ul></div></div></div></div><footer>+</ul></li></ul></div></div></div></div><footer>+" index.html
+sed -i "s/{{BRANCHNAME}}/$INPUT_BRANCHNAME/" index.html
