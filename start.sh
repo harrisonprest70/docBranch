950e075c9e10262ec5fdff221dfe2ebc9f1d13ac
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-06-02 14:38:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,5 +27,6 @@ do
   echo "valore: ${array[n]}"
   sed -i "s+</ul></li></ul></div></div>+<li class=\"toctree-l2\"><a class=\"reference internal\" href=\"${array[n]}/index.html\">${array[n]}</a></li></ul></li></ul></div></div>+" upload/index.html
   n=$n+1
+  cat upload/index.html
 done
 cat upload/index.html
